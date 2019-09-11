3f67ec44989263a4f667a67808ecdae15b8bfdca
Hasnat Md. Abdullah <hasnatabdullah79@gmail.com>
2019-09-11 17:27:51 +0600

    Permission taken from device

---
+++ benna_music/lib/src/base/permissions.dart
@@ -0,0 +1,27 @@
+import 'package:rxdart/rxdart.dart';
+import 'package:permission_handler/permission_handler.dart';
+
+class PermissionsBase{
+  BehaviorSubject _storagePermissionStatus$;
+
+  BehaviorSubject get storagePermissionStatus$ =>
+      _storagePermissionStatus$;
+
+  PermissionsBase(){
+    _storagePermissionStatus$ = BehaviorSubject();
+    requestStoragePermission();
+  }
+  Future requestPermission() async{
+    Map _permission =
+        await PermissionHandler().requestPermissions([
+          PermissionGroup.storage,
+        ]);
+    final PermissionStatus _state = _permission.values.toList()[0];
+    _storagePermissionStatus$.add(_state);
+  }
+
+  void dispose(){
+    _storagePermissionStatus$.close();
+  }
+  
+}
